c64b7a89d44001f9a390e054e41ebaa20a46834b
Devanshu Pote <devanshu.p.pote@gmail.com>
2025-10-14 08:51:49 +0530

    Adding Program File

---
+++ Task1_(Info_Manager)/Task1_(Info_Manager).py
@@ -0,0 +1,10 @@
+name  = "Devanshu"
+full_name = "Devanshu Pote"
+father_name = "Pradiprao Pote"
+age = 20 
+city = "Warud,Maharashtra"
+hobbies = "Playing Chess, Watching Movies"
+
+print("********** Info Manager **********")
+print("Name:",name,"\nFull Name:",full_name,"\nFather Name:",father_name,"\nAge:",
+      age,"\nCity:",city,"\nHobbies:",hobbies)
